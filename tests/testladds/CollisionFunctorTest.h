767517ca91b6d691f47872f258a489263ab05c86
FG-TUM <f.gratl@tum.de>
2021-07-05 13:56:43 +0200

    simple test for the AoSFunctor

---
+++ tests/testladds/CollisionFunctorTest.h
@@ -0,0 +1,11 @@
+/**
+ * @file CollisionFunctorTest.h
+ * @author F. Gratl
+ * @date 05/07/2021
+ */
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+class CollisionFunctorTest : public testing::Test {};
