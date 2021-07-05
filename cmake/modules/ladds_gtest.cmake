2d3a7d8ff92eb6b593fe8d65b199074a53b9e14b
FG-TUM <f.gratl@tum.de>
2021-07-05 13:55:29 +0200

    add gmock to the project

---
+++ cmake/modules/ladds_gtest.cmake
@@ -0,0 +1,23 @@
+message(STATUS "Adding gtest")
+
+include(FetchContent)
+
+# Select https (default) or ssh path.
+set(gTestRepoPath https://github.com/google/googletest.git)
+if (GIT_SUBMODULES_SSH)
+    set(gTestRepoPath git@github.com:google/googletest.git)
+endif ()
+
+FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY ${gTestRepoPath}
+        GIT_TAG release-1.11.0
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+# Disable actual installation of gtest
+option(INSTALL_GTEST "" OFF)
+FetchContent_MakeAvailable(googletest)
+
+# also add the gtest cmake module for cmake functions
+include(GoogleTest)
