3c4b65c01f1a9b458ace76609b3ac4143c623082
Pablo Gómez <contact@pablo.gomez.net>
2021-07-13 17:21:31 +0200

    - Gitignored .cache - Integrated propagator code

---
+++ cmake/modules/propagator.cmake
@@ -0,0 +1,29 @@
+# Propagator library
+message(STATUS "Adding Propagator.")
+
+# Enable ExternalProject CMake module
+include(FetchContent)
+
+# Select https (default) or ssh path.
+set(PropagatorRepoPath https://github.com/Wombatwarrior/BA_space_debris.git)
+if (GIT_SUBMODULES_SSH)
+    set(PropagatorRepoPath git@github.com:Wombatwarrior/BA_space_debris.git)
+endif ()
+
+# set(BUILD_GMOCK OFF)
+# set(INSTALL_GTEST OFF)
+
+FetchContent_Declare(
+        Propagatorfetch
+        GIT_REPOSITORY ${PropagatorRepoPath}
+        GIT_TAG a17a2716a6cdd5b671bc65eb109b12a581d5761f
+)
+
+# Get Propagator source and binary directories from CMake project
+FetchContent_MakeAvailable(Propagatorfetch)
+
+# Disable warnings from the library target
+target_compile_options(debris_sim_lib PRIVATE -w)
+# Disable warnings from included headers
+target_include_directories(debris_sim_lib SYSTEM PUBLIC "${propval}")
+get_target_property(propval debris_sim_lib INTERFACE_INCLUDE_DIRECTORIES)
