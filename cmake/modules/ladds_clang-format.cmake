a24692408a2cf934a1989cc92a38d372d044335f
FG-TUM <f.gratl@tum.de>
2021-09-02 18:02:14 +0200

    reformatted clang-format.cmake and simplified collecting of sources

--- cmake/modules/ladds_clang-format.cmake
+++ cmake/modules/ladds_clang-format.cmake
@@ -1,41 +1,33 @@
 # additional target to perform clang-format run, requires clang-format
-# get all project files
-set(
-    INCLUDE_DIRS
-    "${PROJECT_SOURCE_DIR}/src/"
-    "${PROJECT_SOURCE_DIR}/tests/"
-)
-
-# reset CF_ALL_SOURCE_FILES
-foreach (TMP_PATH ${INCLUDE_DIRS})
-    # search for files for each path in INCLUDE_DIRS and append them to CF_ALL_SOURCE_FILES
-    file(
-        GLOB_RECURSE
-        CF_ALL_SOURCE_FILES_TMP
-        "${TMP_PATH}/*.cpp"
-        "${TMP_PATH}/*.h"
-        "${TMP_PATH}/*.cuh"
-        "${TMP_PATH}/*.cu"
-    )
-    list(APPEND CF_ALL_SOURCE_FILES ${CF_ALL_SOURCE_FILES_TMP})
-endforeach (TMP_PATH)
-
 set(DESIRED_CLANG_FORMAT clang-format-9)
+
 find_program(CLANG_FORMAT NAMES ${DESIRED_CLANG_FORMAT})
 
 if (CLANG_FORMAT)
     message(STATUS "clang format found, added clangformat target")
+
+    # get all project source files
+    foreach (suffix IN ITEMS cpp h)
+        file(
+                GLOB_RECURSE
+                CF_ALL_SOURCE_FILES_TMP
+                "${PROJECT_SOURCE_DIR}/src/*.${suffix}"
+                "${PROJECT_SOURCE_DIR}/tests/*.${suffix}"
+        )
+        list(APPEND CF_ALL_SOURCE_FILES ${CF_ALL_SOURCE_FILES_TMP})
+    endforeach ()
+
     set(dummyfiles)
     foreach (_file ${CF_ALL_SOURCE_FILES})
         string(
-            REPLACE
+                REPLACE
                 "."
                 "_"
                 file_cf
                 ${_file}
         )
         string(
-            REPLACE
+                REPLACE
                 ".."
                 "."
                 file_cf
@@ -43,13 +35,13 @@ if (CLANG_FORMAT)
         )
         set(file_cf ".dummy/cf/${file_cf}_cf")
         add_custom_command(
-            OUTPUT ${file_cf}
-            COMMAND
+                OUTPUT ${file_cf}
+                COMMAND
                 ${CLANG_FORMAT}
                 -style=file
                 -i
                 ${_file}
-            DEPENDS ${_file}
+                DEPENDS ${_file}
         )
         list(APPEND dummyfiles ${file_cf})
     endforeach ()
@@ -57,7 +49,7 @@ if (CLANG_FORMAT)
     add_custom_target(clangformat DEPENDS .dummy/cf/clang_dummy)
 else ()
     message(
-        STATUS
+            STATUS
             "${DESIRED_CLANG_FORMAT} not found, not adding clang format target. Other Versions not supported!"
     )
 endif ()
