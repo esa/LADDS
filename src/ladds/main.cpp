fa9eda45cea073039124e7810ffb373c32f24a7e
FG-TUM <f.gratl@tum.de>
2021-10-19 15:05:13 +0200

    Add timers in main simulation

--- src/ladds/main.cpp
+++ src/ladds/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <autopas/AutoPasDecl.h>
+#include <autopas/utils/Timer.h>
 #include <breakupModel/output/VTKWriter.h>
 #include <satellitePropagator/io/FileOutput.h>
 #include <satellitePropagator/physics/AccelerationAccumulator.h>
@@ -23,8 +24,57 @@
 // Declare the main AutoPas class as extern template instantiation. It is instantiated in AutoPasClass.cpp.
 extern template class autopas::AutoPas<Particle>;
 
+/**
+ * Floating point precision for command line output.
+ */
+constexpr int floatStringPrecision = 3;
+
+/**
+ * Turns the timers into a human readable string.
+ * @param name: The timer's name.
+ * @param timeNS: The time in nano seconds.
+ * @param numberWidth: The precision of the printed number.
+ * @param maxTime: The simulation's total execution time.
+ * @return All information of the timer in a human readable string.
+ *
+ * @note Taken from md-flexible.
+ */
+std::string timerToString(const std::string &name, long timeNS, int numberWidth = 0ul, long maxTime = 0ul) {
+  // only print timers that were actually used
+  if (timeNS == 0) {
+    return "";
+  }
+
+  std::ostringstream ss;
+  ss << std::fixed << std::setprecision(floatStringPrecision) << name << " : " << std::setw(numberWidth) << std::right
+     << timeNS
+     << " ns ("
+     // min width of the representation of seconds is numberWidth - 9 (from conversion) + 4 (for dot and digits after)
+     << std::setw(numberWidth - 5) << ((double)timeNS * 1e-9) << "s)";
+  if (maxTime != 0) {
+    ss << " =" << std::setw(7) << std::right << ((double)timeNS / (double)maxTime * 100) << "%";
+  }
+  ss << std::endl;
+  return ss.str();
+}
+
 int main(int argc, char **argv) {
+  // might be moved somewhere else if we add proper object orientation for the main logic
+  struct Timers {
+    autopas::utils::Timer total{};
+    autopas::utils::Timer initialization{};
+    autopas::utils::Timer simulation{};
+    autopas::utils::Timer integrator{};
+    autopas::utils::Timer collisionDetection{};
+    autopas::utils::Timer containerUpdate{};
+    autopas::utils::Timer output{};
+  } __attribute__((aligned(128))) timers;
+
+  timers.total.start();
+  timers.initialization.start();
+
   Logger logger;
+
   logger.get()->set_level(spdlog::level::debug);
 
   // Default config path
@@ -94,19 +144,28 @@ int main(int argc, char **argv) {
 
   logger.log(Logger::Level::info, "Number of particles: {}", autopas.getNumberOfParticles());
 
+  timers.initialization.stop();
+
   // main-loop skeleton
+  timers.simulation.start();
   for (size_t i = 0; i < iterations; ++i) {
     // update positions
+    timers.integrator.start();
     integrator->integrate(false);
+    timers.integrator.stop();
 
     // TODO MPI: handle particle exchange between ranks
+    timers.containerUpdate.start();
     // (potentially) update the internal data structure and collect particles which are leaving the container.
     const auto [escapedParticles, containerUpdated] = autopas.updateContainer();
+    timers.containerUpdate.stop();
 
     if (not escapedParticles.empty()) {
       logger.log(Logger::Level::err, "Particles are escaping! \n{}", escapedParticles);
     }
     // TODO Check for particles that burn up
+
+    timers.collisionDetection.start();
     // pairwise interaction
     CollisionFunctor collisionFunctor(cutoff);
     autopas.iteratePairwise(&collisionFunctor);
@@ -115,9 +174,11 @@ int main(int argc, char **argv) {
     for (const auto &[p1, p2] : collisions) {
       logger.log(Logger::Level::debug, "{} | {}", p1->getID(), p2->getID());
     }
+    timers.collisionDetection.stop();
 
     // TODO insert breakup model here
 
+    timers.output.start();
     // Visualization:
     if (i % vtkWriteFrequency == 0) {
       VTKWriter vtkWriter("output_" + std::to_string(i) + ".vtu");
@@ -128,7 +189,28 @@ int main(int argc, char **argv) {
       }
       vtkWriter.printResult(allParticles);
     }
+    timers.output.stop();
   }
+  timers.simulation.stop();
+  timers.total.stop();
+
+  // dump all timers
+  const auto timeTotal = timers.total.getTotalTime();
+  const auto timeSim = timers.simulation.getTotalTime();
+  const auto maximumNumberOfDigits = static_cast<int>(std::to_string(timeTotal).length());
+  std::cout << timerToString("Total                   ", timeTotal, maximumNumberOfDigits);
+  std::cout << timerToString(
+      "  Initialization        ", timers.initialization.getTotalTime(), maximumNumberOfDigits, timeTotal);
+  std::cout << timerToString("  Simulation            ", timeSim, maximumNumberOfDigits, timeTotal);
+  std::cout << timerToString(
+      "    Integrator          ", timers.integrator.getTotalTime(), maximumNumberOfDigits, timeSim);
+  std::cout << timerToString(
+      "    Collision detection ", timers.collisionDetection.getTotalTime(), maximumNumberOfDigits, timeSim);
+  std::cout << timerToString(
+      "    Container update    ", timers.containerUpdate.getTotalTime(), maximumNumberOfDigits, timeSim);
+  std::cout << timerToString(
+      "  Output                ", timers.output.getTotalTime(), maximumNumberOfDigits, timeTotal);
+  std::cout << timerToString("One iteration           ", timeSim / iterations, maximumNumberOfDigits, timeTotal);
 
   return 0;
 }
