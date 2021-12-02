8154208a4cdc5ebc202cbf3af6b17fbd78ffd819
albertNos <92428351+albertNos@users.noreply.github.com>
2021-12-02 17:41:46 +0100

    Update SafeInsertion.h; Collision detection
    Co-authored-by: FG-TUM <FG-TUM@users.noreply.github.com>

--- src/ladds/SafeInsertion.h
+++ src/ladds/SafeInsertion.h
@@ -22,16 +22,17 @@ namespace SafeInsertion {
 std::vector<Particle> insert(autopas::AutoPas<Particle> &autopas, const std::vector<Particle> &newSatellites, double cutoff) {
   std::vector<Particle> delayedInsertion = {};
 
-  for (auto &nSat : newSatellites) {
+  const double collisionRadius = 2 * cutoff;
+  const double collisionRadiusSquared = collisionRadius * collisionRadius;
+  const std::array<double, 3> boxSpan = {collisionRadius, collisionRadius, collisionRadius};
+  for (const auto &nSat : newSatellites) {
     // only insert satellites, if they have a reasonable distance to other satellites
     bool collisionFree = true;
-    double collisionRadius = 2 * cutoff;
-    std::array<double, 3> boxSpan = {collisionRadius, collisionRadius, collisionRadius};
-    std::array<double, 3> lowCorner = autopas::utils::ArrayMath::sub(nSat.getPosition(), boxSpan);
-    std::array<double, 3> highCorner = autopas::utils::ArrayMath::add(nSat.getPosition(), boxSpan);
-    for (auto iter = autopas.getRegionIterator(lowCorner, highCorner); iter.isValid(); ++iter) {
+    const std::array<double, 3> lowCorner = autopas::utils::ArrayMath::sub(nSat.getPosition(), boxSpan);
+    const std::array<double, 3> highCorner = autopas::utils::ArrayMath::add(nSat.getPosition(), boxSpan);
+    for (auto iter = autopas.getRegionIterator(lowCorner, highCorner); iter.isValid() and collisionFree; ++iter) {
       std::array<double, 3> diff = autopas::utils::ArrayMath::sub(nSat.getPosition(), iter->getPosition());
-      collisionFree = collisionFree && (autopas::utils::ArrayMath::dot(diff, diff) > collisionRadius * collisionRadius);
+      collisionFree = autopas::utils::ArrayMath::dot(diff, diff) > collisionRadiusSquared;
     }
     if (collisionFree) {
       autopas.addParticle(nSat);
