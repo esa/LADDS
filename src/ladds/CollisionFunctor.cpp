d636a180f55034fc17723244b334c6a8edf53507
Pablo Gómez <contact@pablo-gomez.net>
2021-12-07 16:25:36 +0100

    Added exception if wrong kernel is used

--- src/ladds/CollisionFunctor.cpp
+++ src/ladds/CollisionFunctor.cpp
@@ -140,6 +140,9 @@ void CollisionFunctor::SoAFunctorVerlet(autopas::SoAView<SoAArraysType> soa,
 
 void CollisionFunctor::SoAKernel(
     size_t i, size_t j, autopas::SoAView<SoAArraysType> &soa1, autopas::SoAView<SoAArraysType> &soa2, bool newton3) {
+
+  throw std::runtime_error("SoA kernel not up to date with AoS Kernel as it lacks the new linear interpolation distance.");
+    
   // get pointers to the SoAs
   const auto *const __restrict x1ptr = soa1.template begin<Particle::AttributeNames::posX>();
   const auto *const __restrict y1ptr = soa1.template begin<Particle::AttributeNames::posY>();
