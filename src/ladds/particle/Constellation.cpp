ab715964a0e5bb05047f83c499cf1d14805ffe5a
albertNos <albert.noswitz@tum.de>
2021-12-14 17:46:00 +0100

    notebook creates data differently, ladds reads new different data

--- src/ladds/particle/Constellation.cpp
+++ src/ladds/particle/Constellation.cpp
@@ -14,24 +14,15 @@
 
 int Constellation::seed = 10;
 
-Constellation::Constellation(const std::string &constellation_data_str, size_t interval, double altitudeDeviation)
+Constellation::Constellation(const YAML::Node &constellationConfig, size_t interval, double altitudeDeviation)
         : interval(interval), altitudeDeviation(altitudeDeviation) {
-    // split the 3 comma seperated arguments
-    auto seperator1 = constellation_data_str.find(',', 0);
-    auto seperator2 = constellation_data_str.find(',', seperator1 + 1);
 
-    // argDirPath s1 argStartTime s2  argDuration    // n: 4
-    // a   b   c   d   ,   e   f   ,   g   h   i   // n: 7-4-1=2
-    // 0   1   2   3   4   5   6   7   8   9   10  // n: 11-7-1=3
-    std::string argConstellationName = constellation_data_str.substr(0, seperator1);
-    std::string argStartTime = constellation_data_str.substr(seperator1 + 1, seperator2 - seperator1 - 1);
-    std::string argDuration =
-            constellation_data_str.substr(seperator2 + 1, constellation_data_str.size() - seperator2 - 1);
+    std::string constellationName = constellationConfig["constellation"]["name"].as<std::string>();
 
     // set variables using 3 args
     std::vector<Particle> sats =
-            readDatasetConstellation(std::string(DATADIR) + argConstellationName + "/pos_" + argConstellationName + ".csv",
-                                     std::string(DATADIR) + argConstellationName + "/v_" + argConstellationName + ".csv");
+            readDatasetConstellation(std::string(DATADIR) + constellationName + "/pos_" + constellationName + ".csv",
+                                     std::string(DATADIR) + constellationName + "/v_" + constellationName + ".csv");
 
     generator.seed(seed++);
     distribution = std::normal_distribution<double>(0, this->altitudeDeviation);
@@ -42,24 +33,18 @@ Constellation::Constellation(const std::string &constellation_data_str, size_t i
         satellites.push_back(sats[i]);
     }
 
-    startTime = std::stoi(argStartTime);
-    duration = std::stoi(argDuration);
 
-    std::ifstream shellParameters(std::string(DATADIR) + argConstellationName + "/shells_" + argConstellationName +
-                                  ".txt");
-    std::string tmp_string;
-    std::getline(shellParameters, tmp_string);
-    double altitude, inclination, nPlanes, satsPerPlane;
-    while (!tmp_string.empty()) {
-        std::istringstream numStream(tmp_string);
-        numStream >> altitude;
-        numStream >> inclination;
-        numStream >> nPlanes;
-        numStream >> satsPerPlane;
-        shells.emplace_back<std::array<double, 4>>({altitude, inclination, nPlanes, satsPerPlane});
-        std::getline(shellParameters, tmp_string);
+    startTime = constellationConfig["constellation"]["startTime"].as<int>();
+    duration = constellationConfig["constellation"]["duration"].as<int>();
+
+    int nShells = constellationConfig["constellation"]["nShells"].as<int>();
+    for(int i = 1; i<=nShells;i++) {
+        std::string attribute = "shell" + std::to_string(i);
+        shells.emplace_back<std::array<double, 4>>({constellationConfig[attribute]["altitude"].as<double>(),
+                                                    constellationConfig[attribute]["inclination"].as<double>(),
+                                                    constellationConfig[attribute]["nPlanes"].as<double>(),
+                                                    constellationConfig[attribute]["nSats"].as<double>()});
     }
-    shellParameters.close();
 
     // determine times when each shell has its deployment started
     double timestamp = 0;
--- src/ladds/particle/Constellation.h
+++ src/ladds/particle/Constellation.h
@@ -11,27 +11,28 @@
 #include <deque>
 #include <iostream>
 
+#include <yaml-cpp/yaml.h>
+
 #include "ladds/particle/Particle.h"
 
 /**
- * The Constellation class contains a collection of Particles that inserts these particles to
- * the simulation over time based on parameters a Constellation object is constructed with.
- * The object is constructed from a data string that consists of the comma seperated
- * arguments: directory path (directory with constellation information files), start time
- * (time when constellation is inserted), duration (time span of insertion)
+ * The Constellation class contains a collection of Particles that inserts these particles into
+ * the simulation over time based on .csv files and a .yaml file as created by the
+ * ConstellationGeneration notebook
  */
 class Constellation {
 public:
     /**
      * Constructs a constellation object
-     * @param constellation : string formatted as a comma seperated 3-tuple containing
-     * a path to a directory with constellation information, the time the constellation
-     * deployment is started, and the duration of the duration
+     * @param constellationConfig : YAML::Node object with the constellation data. Valid
+     * constellation data can be created using the ConstellationGeneration notebook
+     * and must be in the projects data folder
      * @param interval : the interval of satellites being added to the simulation is
      * passed for internal logic
-     * @param variance :
+     * @param altitudeDeviation : used to create satellites with normally distributed
+     * altitudes. Equals the standard deviation of a normal distribution
      */
-    Constellation(const std::string &constellation_data_str, size_t interval, double altitudeDeviation);
+    Constellation(const YAML::Node &constellationConfig, size_t interval, double altitudeDeviation);
     /**
      * determines which satellites are being added to the simulation by adding each shell
      * within a time span proportional to the shells size. shells are added plane by plane
@@ -144,7 +145,7 @@ private:
 
     /**
      * deviation parameter of the normal distribution that determines the deviation
-     * of the satellites base altitude
+     * of the satellites base altitude. Equals the standard deviation of a normal distribution
      */
     double altitudeDeviation;
 
@@ -163,7 +164,7 @@ private:
     std::mt19937 generator;
 
     /**
-     * normal distribution that determines the deviation of the satellites base
+     * normal distribution that determines the random deviation from the satellites base
      * altitude. uses altitudeDeviation as parameter
      */
     std::normal_distribution<double> distribution;
