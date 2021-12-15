b3171011668a3fc95065eb497ce118e26b28c3b3
FG-TUM <f.gratl@tum.de>
2021-12-15 13:10:06 +0100

    Simple version of HDF5 files via HighFive

--- src/ladds/io/HDF5Writer.cpp
+++ src/ladds/io/HDF5Writer.cpp
@@ -7,30 +7,22 @@
 #include "HDF5Writer.h"
 
 void HDF5Writer::write(size_t iteration, const AutoPas_t &autopas) {
-  struct point3D {
-    double x, y, z;
-  };
-
-  //  std::vector<data_t> data;
-  //  data.reserve(autopas.getNumberOfParticles());
-  std::vector<point3D> positions;
-  std::vector<point3D> velocities;
-  std::vector<size_t> ids;
-  positions.reserve(autopas.getNumberOfParticles());
-  velocities.reserve(autopas.getNumberOfParticles());
-  ids.reserve(autopas.getNumberOfParticles());
+  std::vector<ParticleData> data;
+  data.reserve(autopas.getNumberOfParticles());
 
   for (const auto &particle : autopas) {
     const auto &pos = particle.getR();
-    positions.emplace_back<point3D>({pos[0], pos[1], pos[2]});
     const auto &vel = particle.getV();
-    velocities.emplace_back<point3D>({vel[0], vel[1], vel[2]});
-    ids.push_back(particle.getID());
+    data.emplace_back<ParticleData>({static_cast<float>(pos[0]),
+                                     static_cast<float>(pos[1]),
+                                     static_cast<float>(pos[2]),
+                                     static_cast<float>(vel[0]),
+                                     static_cast<float>(vel[1]),
+                                     static_cast<float>(vel[2]),
+                                     particle.getID()});
   }
 
   const auto group = "Iteration" + std::to_string(iteration);
-  _file.writeDataset(positions, group + "/Positions");
-  _file.writeDataset(velocities, group + "/Velocities");
-  _file.writeDataset(ids, group + "/IDs");
+  _file.writeDataset(data, group + "/Particles", ParticleDataH5Type);
   _file.writeAttribute(iteration, "IterationNr", group);
 }
--- src/ladds/io/HDF5Writer.h
+++ src/ladds/io/HDF5Writer.h
@@ -14,10 +14,38 @@
 
 class HDF5Writer {
  public:
-  explicit HDF5Writer(const std::string &filename) : _file(filename, h5pp::FilePermission::REPLACE) {}
+  explicit HDF5Writer(const std::string &filename)
+      : _file(filename, h5pp::FilePermission::REPLACE),
+        ParticleDataH5Type(H5Tcreate(H5T_COMPOUND, sizeof(ParticleData))) {
+    // Register ParticleData type with HDF5
+    H5Tinsert(ParticleDataH5Type, "rx", HOFFSET(ParticleData, rx), H5T_NATIVE_FLOAT);
+    H5Tinsert(ParticleDataH5Type, "ry", HOFFSET(ParticleData, ry), H5T_NATIVE_FLOAT);
+    H5Tinsert(ParticleDataH5Type, "rz", HOFFSET(ParticleData, rz), H5T_NATIVE_FLOAT);
+    H5Tinsert(ParticleDataH5Type, "vx", HOFFSET(ParticleData, vx), H5T_NATIVE_FLOAT);
+    H5Tinsert(ParticleDataH5Type, "vy", HOFFSET(ParticleData, vy), H5T_NATIVE_FLOAT);
+    H5Tinsert(ParticleDataH5Type, "vz", HOFFSET(ParticleData, vz), H5T_NATIVE_FLOAT);
+    H5Tinsert(ParticleDataH5Type, "id", HOFFSET(ParticleData, id), H5T_NATIVE_ULONG);
+    _file.setCompressionLevel(9);
+  }
 
   void write(size_t iteration, const AutoPas_t &autopas);
 
  private:
+  /**
+   * This represents one line of data in the HDF5 file.
+   */
+  struct ParticleData {
+    float rx, ry, rz;
+    float vx, vy, vz;
+    size_t id;
+  };
+
+  /**
+   * Actual file that will be created. All of the data this writer gets ends up in this one file.
+   */
   h5pp::File _file;
+  /**
+   * Object representing the wrapper type HDF5 uses to understand the non-trivial struct ParticleData.
+   */
+  h5pp::hid::h5t ParticleDataH5Type;
 };
