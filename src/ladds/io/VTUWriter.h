fbff6be9712b7ee5ea42f6a03f7489ab5b7d3546
FG-TUM <f.gratl@tum.de>
2021-12-15 15:52:32 +0100

    doc

--- src/ladds/io/VTUWriter.h
+++ src/ladds/io/VTUWriter.h
@@ -13,6 +13,12 @@ namespace VTUWriter {
  * Write a vtk file with the current state of the simulation.
  */
 void writeVTK(size_t iteration, const AutoPas_t &autopas);
+/**
+ * Write a vtk file in legacy table style (no xml) but with binary storage layout.
+ * Without compression this is probably the smallest output file possible.
+ * @param iteration
+ * @param autopas
+ */
 void writeLegacyVTKBinary(size_t iteration, const AutoPas_t &autopas);
 
 }  // namespace VTUWriter
