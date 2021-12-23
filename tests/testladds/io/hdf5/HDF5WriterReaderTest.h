448bef1428f8da82bb934d2d67cd88ecc9938115
FG-TUM <f.gratl@tum.de>
2021-12-23 20:22:10 +0100

    tests for hdf5

---
+++ tests/testladds/io/hdf5/HDF5WriterReaderTest.h
@@ -0,0 +1,11 @@
+/**
+ * @file HDF5WriterReaderTest.h
+ * @author F. Gratl
+ * @date 23.12.2021
+ */
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+class HDF5WriterReaderTest : public testing::Test {};
