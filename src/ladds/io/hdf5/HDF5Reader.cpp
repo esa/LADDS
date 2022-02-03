eda5f977b7b760f13b2b45afdcfb7d8fe1e9cb69
FG-TUM <f.gratl@tum.de>
2022-02-03 13:04:57 +0100

    do nothing in HDF5Reader::readLastIterationNr() if HDF5 is not used

--- src/ladds/io/hdf5/HDF5Reader.cpp
+++ src/ladds/io/hdf5/HDF5Reader.cpp
@@ -47,10 +47,11 @@ std::vector<HDF5Writer::CollisionInfo> HDF5Reader::readCollisions(size_t iterati
 }
 
 size_t HDF5Reader::readLastIterationNr() {
+  std::vector<size_t> allIterations;
+#ifdef LADDS_HDF5
   const auto groupStrLength = std::string(HDF5Writer::groupParticleData).size();
   // gather all ID datasets. They contain the iteration number in their path
   auto allIdDatasets = file.findDatasets(HDF5Writer::datasetParticleIDs);
-  std::vector<size_t> allIterations;
   allIterations.reserve(allIdDatasets.size());
   // extract all iteration numbers as size_t
   std::transform(allIdDatasets.begin(), allIdDatasets.end(), std::back_inserter(allIterations), [&](auto &datasetName) {
@@ -60,5 +61,6 @@ size_t HDF5Reader::readLastIterationNr() {
   });
   // sort numerically and return the highest
   std::sort(allIterations.begin(), allIterations.end());
+#endif
   return allIterations.back();
 }
