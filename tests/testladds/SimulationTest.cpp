276d24e2ea8e5c23f6cb465a9d84c047d28b9d8a
FG-TUM <f.gratl@tum.de>
2022-05-23 17:05:24 +0200

    adapt tests to new namespace

--- tests/testladds/SimulationTest.cpp
+++ tests/testladds/SimulationTest.cpp
@@ -36,46 +36,48 @@ TEST_F(SimulationTest, testInsertionOverlap) {
    */
   const std::vector<size_t> expectedDelayedParticles{2, 1, 1, 0, 0, 0};
 
-  const std::vector<Particle> initialSatellites{Particle({6871., 0., 0.},
-                                                         {0, 4.8958309146899, 5.83462408131549},
-                                                         1,
-                                                         "initial 1",
-                                                         Particle::ActivityState::passive,
-                                                         1.,
-                                                         1.,
-                                                         Particle::calculateBcInv(0., 1., 1., 2.2)),
-                                                Particle({6870.99577848984, 4.89582991243564, 5.83462288687537},
-                                                         {-0.00844301944979238, 4.89582790671436, 5.83462049654983},
-                                                         2,
-                                                         "initial 2",
-                                                         Particle::ActivityState::passive,
-                                                         1.,
-                                                         1.,
-                                                         Particle::calculateBcInv(0., 1., 1., 2.2))};
+  const std::vector<LADDS::Particle> initialSatellites{
+      LADDS::Particle({6871., 0., 0.},
+                      {0, 4.8958309146899, 5.83462408131549},
+                      1,
+                      "initial 1",
+                      LADDS::Particle::ActivityState::passive,
+                      1.,
+                      1.,
+                      LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)),
+      LADDS::Particle({6870.99577848984, 4.89582991243564, 5.83462288687537},
+                      {-0.00844301944979238, 4.89582790671436, 5.83462049654983},
+                      2,
+                      "initial 2",
+                      LADDS::Particle::ActivityState::passive,
+                      1.,
+                      1.,
+                      LADDS::Particle::calculateBcInv(0., 1., 1., 2.2))};
   for (const auto &s : initialSatellites) {
     autopas->addParticle(s);
   }
-  const std::vector<Particle> newSatellites{Particle({6871., 0., 0.},
-                                                     {0, 4.8958309146899, 5.83462408131549},
-                                                     1,
-                                                     "new 1",
-                                                     Particle::ActivityState::passive,
-                                                     0,
-                                                     0,
-                                                     Particle::calculateBcInv(0., 1., 1., 2.2)),
-                                            Particle({6870.99577848984, 4.89582991243564, 5.83462288687537},
-                                                     {-0.00844301944979238, 4.89582790671436, 5.83462049654983},
-                                                     4,
-                                                     "new 2",
-                                                     Particle::ActivityState::passive,
-                                                     1.,
-                                                     1.,
-                                                     Particle::calculateBcInv(0., 1., 1., 2.2))};
+  const std::vector<LADDS::Particle> newSatellites{
+      LADDS::Particle({6871., 0., 0.},
+                      {0, 4.8958309146899, 5.83462408131549},
+                      1,
+                      "new 1",
+                      LADDS::Particle::ActivityState::passive,
+                      0,
+                      0,
+                      LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)),
+      LADDS::Particle({6870.99577848984, 4.89582991243564, 5.83462288687537},
+                      {-0.00844301944979238, 4.89582790671436, 5.83462049654983},
+                      4,
+                      "new 2",
+                      LADDS::Particle::ActivityState::passive,
+                      1.,
+                      1.,
+                      LADDS::Particle::calculateBcInv(0., 1., 1., 2.2))};
 
   auto constellationCutoff = config["io"]["constellationCutoff"].as<double>();
 
   ASSERT_EQ(autopas->getNumberOfParticles(), 2) << "Container initialized wrong!";
-  std::vector<Particle> delayedSatellites = newSatellites;
+  std::vector<LADDS::Particle> delayedSatellites = newSatellites;
   // simulate some iterations and check delays against expectations from above
   for (size_t iteration = 0; iteration < expectedDelayedParticles.size(); ++iteration) {
     auto escapedParticles = autopas->updateContainer();
@@ -98,28 +100,28 @@ TEST_F(SimulationTest, testBurnUp) {
   auto [csvWriter, accumulator, integrator] = simulation.initIntegrator(*autopas, *configReader);
   const auto &minAltitude = Physics::R_EARTH + configReader->get<double>("sim/minAltitude");
   // initialize a particle 1km above burn up radius with a trajectory towards earth
-  autopas->addParticle(Particle({minAltitude + 1., 0., 0.},
-                                {-10., 0., 0.},
-                                0,
-                                "1km above ground towards earth",
-                                Particle::ActivityState::passive,
-                                1.,
-                                1.,
-                                Particle::calculateBcInv(0., 1., 1., 2.2)));
+  autopas->addParticle(LADDS::Particle({minAltitude + 1., 0., 0.},
+                                       {-10., 0., 0.},
+                                       0,
+                                       "1km above ground towards earth",
+                                       LADDS::Particle::ActivityState::passive,
+                                       1.,
+                                       1.,
+                                       LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)));
   // initialize a particle 1km above burn up radius with a trajectory away from earth
   // different position to avoid any interferences
-  autopas->addParticle(Particle({0., minAltitude + 1., 0.},
-                                {0., 10., 0.},
-                                1,
-                                "1km above ground away from earth",
-                                Particle::ActivityState::passive,
-                                1.,
-                                1.,
-                                Particle::calculateBcInv(0., 1., 1., 2.2)));
+  autopas->addParticle(LADDS::Particle({0., minAltitude + 1., 0.},
+                                       {0., 10., 0.},
+                                       1,
+                                       "1km above ground away from earth",
+                                       LADDS::Particle::ActivityState::passive,
+                                       1.,
+                                       1.,
+                                       LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)));
   ASSERT_EQ(autopas->getNumberOfParticles(), 2) << "Initial particles not found!";
 
   // dummy because interface requires it
-  std::vector<Constellation> constellations{};
+  std::vector<LADDS::Constellation> constellations{};
   // do one iteration where the burnup is expected to happen
   simulation.simulationLoop(*autopas, *integrator, constellations, *configReader);
 
@@ -140,29 +142,29 @@ TEST_F(SimulationTest, testTimestepsPerCollisionDetection) {
     autopas->deleteAllParticles();
     auto [csvWriter, accumulator, integrator] = simulation.initIntegrator(*autopas, *configReader);
     const auto &minAltitude = Physics::R_EARTH + configReader->get<double>("sim/minAltitude");
-    autopas->addParticle(Particle({minAltitude + 100., 20., 0.},
-                                  {0., -10., 0.},
-                                  0,
-                                  "A",
-                                  Particle::ActivityState::passive,
-                                  1.,
-                                  1.,
-                                  Particle::calculateBcInv(0., 1., 1., 2.2)));
-    autopas->addParticle(Particle({minAltitude + 100., -20, 0.},
-                                  {0., 10., 0.},
-                                  1,
-                                  "B",
-                                  Particle::ActivityState::passive,
-                                  1.,
-                                  1.,
-                                  Particle::calculateBcInv(0., 1., 1., 2.2)));
+    autopas->addParticle(LADDS::Particle({minAltitude + 100., 20., 0.},
+                                         {0., -10., 0.},
+                                         0,
+                                         "A",
+                                         LADDS::Particle::ActivityState::passive,
+                                         1.,
+                                         1.,
+                                         LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)));
+    autopas->addParticle(LADDS::Particle({minAltitude + 100., -20, 0.},
+                                         {0., 10., 0.},
+                                         1,
+                                         "B",
+                                         LADDS::Particle::ActivityState::passive,
+                                         1.,
+                                         1.,
+                                         LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)));
     ASSERT_EQ(autopas->getNumberOfParticles(), 2) << "Initial particles not found!";
 
     configReader->setValue("sim/iterations", iterations);
     configReader->setValue("sim/conjunctionThreshold", 0.003);
     configReader->setValue("sim/timestepsPerCollisionDetection", 2);
     // dummy because interface requires it
-    std::vector<Constellation> constellations{};
+    std::vector<LADDS::Constellation> constellations{};
     const auto conjunctions = simulation.simulationLoop(*autopas, *integrator, constellations, *configReader);
     constexpr auto errorString{"Unexpected number of conjunctions in iterations "};
     switch (iterations) {
@@ -190,29 +192,29 @@ TEST_F(SimulationTest, testTimestepsPerCollisionDetection) {
 TEST_P(SimulationTest, testCheckedInsert) {
   const auto &[posTestParticle, positionIsSafe] = GetParam();
 
-  autopas->addParticle(Particle(testCheckedInsertParticlePos,
-                                zeroVec,
-                                0,
-                                "existing",
-                                Particle::ActivityState::passive,
-                                1.,
-                                1.,
-                                Particle::calculateBcInv(0., 1., 1., 2.2)));
+  autopas->addParticle(LADDS::Particle(testCheckedInsertParticlePos,
+                                       zeroVec,
+                                       0,
+                                       "existing",
+                                       LADDS::Particle::ActivityState::passive,
+                                       1.,
+                                       1.,
+                                       LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)));
 
   // particle that will be inserted
-  Particle p1{posTestParticle,
-              zeroVec,
-              1,
-              "tester",
-              Particle::ActivityState::passive,
-              1.,
-              1.,
-              Particle::calculateBcInv(0., 1., 1., 2.2)};
+  LADDS::Particle p1{posTestParticle,
+                     zeroVec,
+                     1,
+                     "tester",
+                     LADDS::Particle::ActivityState::passive,
+                     1.,
+                     1.,
+                     LADDS::Particle::calculateBcInv(0., 1., 1., 2.2)};
 
   const auto escapedParticles = autopas->updateContainer();
   ASSERT_TRUE(escapedParticles.empty()) << "Test setup faulty!";
 
-  std::vector<Particle> delayedParticles = simulation.checkedInsert(*autopas, {p1}, constellationCutoff);
+  std::vector<LADDS::Particle> delayedParticles = simulation.checkedInsert(*autopas, {p1}, constellationCutoff);
   // if the position is safe the particle is inserted and there are now two. Otherwise, we remain with one.
   EXPECT_EQ(autopas->getNumberOfParticles(), positionIsSafe ? 2 : 1);
   // if the position is considered safe for insertion there should be no delayed particle
