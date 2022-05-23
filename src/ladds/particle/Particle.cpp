315858d9fe6587e8d0f0e08e174960ec069abc6e
Pablo Gómez <contact@pablo-gomez.net>
2022-05-23 13:11:20 +0200

    Update src/ladds/particle/Particle.cpp

--- src/ladds/particle/Particle.cpp
+++ src/ladds/particle/Particle.cpp
@@ -17,7 +17,8 @@ double Particle::calculateBcInv(double bstar, double mass, double radius, double
     // or via bstar
     // @note see https://en.wikipedia.org/wiki/BSTAR
     // B* == p_0 * c_D * A / (2 m) == bc_inv * p_0 / 2
-    // Thus we factor out p0 (=0.1570), convert to m (original bstar is in Earth radii) and the factor 2
+    // Thus we factor out p0 (=0.1570), convert to m (original bstar is in Earth radii) and the factor 2 comes
+    // from the propagator still having a 1/2 multiplication term included which is already accounted for in BSTAR
     return 2.0 * bstar * Physics::R_EARTH / 0.1570;
   }
 }
